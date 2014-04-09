3e2f591ddb81f42ba451d9a9c1960ad87a0ef6b3
dvdrtrgn <dvdrtrgn@gmail.com>
2014-04-09 16:48:01 -0500

    tidy load

--- load.js
+++ load.js
@@ -4,73 +4,73 @@
 var Data, Load, Test, Glob;
 
 (function ($, M) {
-'use strict';
-var G, Load = {};
+    'use strict';
+    var G, Load = {};
 
-G = W.G = new Global('Globals');
-W.Data = new Global('Data');
-W.Test = $.Callbacks();
-W.debug = 1;
-C.groupCollapsed('load routines');
+    G = W.G = new Global('Globals');
+    W.Data = new Global('Data');
+    W.Test = $.Callbacks();
+    W.debug = 1;
+    C.groupCollapsed('load routines');
 
-$.extend(G, { /// all stubs terminated
-    dir: ROOT.dir + '/',
-    lib: ROOT.lib + '/',
-    loc: ROOT.dir + '/lib/',
-    src: ROOT.dir + '/scripts/',
-    dat: ROOT.dir + '/data/',
-    postfix: function () {},
-});
+    $.extend(G, { /// all stubs terminated
+        dir: ROOT.dir + '/',
+        lib: ROOT.lib + '/',
+        loc: ROOT.dir + '/lib/',
+        src: ROOT.dir + '/scripts/',
+        dat: ROOT.dir + '/data/',
+        postfix: function () {},
+    });
 
-Load.base = {
-    both: [
-    G.lib + 'underscore/js-1.4.4/underscore.js',
-    G.lib + 'underscore/string-2.3.0/underscore.string.js',
-    G.loc + '_utils.js',
-    ],
-};
+    Load.base = {
+        both: [
+        G.lib + 'underscore/js-1.4.4/underscore.js',
+        G.lib + 'underscore/string-2.3.0/underscore.string.js',
+        G.loc + '_utils.js',
+        ],
+    };
 
-Load.prep = {
-    test: W.isIE,
-    yep: [
-    G.lib + 'ie/nwmatcher.min.js', /*       css3 selector help      */
-    G.lib + 'ie/rem.min.js', /*             css rem polyfill        */
-    G.lib + 'ie/selectivizr-min.js', /*     css3 polyfill           */
-    G.lib + 'ie/split.js', /*               string.regexp polyfill  */
-    ],
-    complete: function () {
-        if ($.browser.msie) {
-            var vers = parseInt($.browser.version, 10);
-            if (vers < 10) {
-                W.location = 'images/plates/broke.png';
-            } else {
-                W.setTimeout(function () {
-                    W.Platter.broke();
-                }, 999);
+    Load.prep = {
+        test: W.isIE,
+        yep: [
+        G.lib + 'ie/nwmatcher.min.js', /*       css3 selector help      */
+        G.lib + 'ie/rem.min.js', /*             css rem polyfill        */
+        G.lib + 'ie/selectivizr-min.js', /*     css3 polyfill           */
+        G.lib + 'ie/split.js', /*               string.regexp polyfill  */
+        ],
+        complete: function () {
+            if ($.browser.msie) {
+                var vers = parseInt($.browser.version, 10);
+                if (vers < 10) {
+                    W.location = 'images/plates/broke.png';
+                } else {
+                    W.setTimeout(function () {
+                        W.Platter.broke();
+                    }, 999);
+                }
             }
-        }
-        if (W.location.hash || W.location.search) {
-            W.location = W.location.pathname;
-        }
-    },
-};
+            if (W.location.hash || W.location.search) {
+                W.location = W.location.pathname;
+            }
+        },
+    };
 
-Load.main = {
-    both: [
-    G.dir + 'load_lib.js',
-    G.dir + 'load_raw.js',
-    G.dir + 'load_src.js',
-    G.dir + '_main.js',
-    G.dir + '_ideas.js',
-    ],
-    complete: function () {
-        C.groupEnd();
-        C.info('Load.main init @ ' + Date() + ' debug:', W.debug); //, self.mode
-        $(W.inits);
-    },
-};
+    Load.main = {
+        both: [
+        G.dir + 'load_lib.js',
+        G.dir + 'load_raw.js',
+        G.dir + 'load_src.js',
+        G.dir + '_main.js',
+        G.dir + '_ideas.js',
+        ],
+        complete: function () {
+            C.groupEnd();
+            C.info('Load.main init @ ' + Date() + ' debug:', W.debug); //, self.mode
+            $(W.inits);
+        },
+    };
 
-M.load([Load.base, Load.prep, Load.main]);
-Glob = G;
+    M.load([Load.base, Load.prep, Load.main]);
+    Glob = G;
 
 }(jQuery, Modernizr));
