2a4923e05715a3d76023037c6eae1b14bfdb1c7f
dvdrtrgn <dvdrtrgn@gmail.com>
2014-04-09 17:20:23 -0500

    trim fixes

--- load_src.js
+++ load_src.js
@@ -1,28 +1,35 @@
 /*jslint es5:true, white:false */
-/*globals Modernizr, window */
+/*globals C, W, $, Modernizr */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 Modernizr.load([{
-    test: window.location.host !== 'localhost:8000',
+    test: W.location.host !== 'localhost:8000',
     yep: [
-        '//cloud.typography.com/6819872/620964/css/fonts.css', // Normal
-        //'//cloud.typography.com/6819872/633184/css/fonts.css', // ScrnSmrt
+        '//cloud.typography.com/6819872/620964/css/fonts.css', /* Normal */
+        /*'//cloud.typography.com/6819872/633184/css/fonts.css',  ScrnSmrt */
     ],
     nope: [
         'lib/archer.css',
         'lib/archer.itl.css',
     ],
     both: [
-        'scripts/banner.js', //     + css
+        'scripts/banner.js', /*     + css    */
         'scripts/backer.js',
-        'scripts/signs.js', //      + css
-        'scripts/platter.js', //    + css
+        'scripts/signs.js', /*      + css    */
+        'scripts/platter.js', /*    + css    */
         'scripts/player.js',
         'scripts/region.js',
-        'scripts/seasons.js', //    + css
+        'scripts/seasons.js', /*    + css    */
         'scripts/space.js',
-        'scripts/stage.js', //      + css
-        'scripts/vehicle.js', //    + css
-        'scripts/points.js', //     + css
-        'scripts/fixes.js', //      + css
-        ],
-    }]);
+        'scripts/stage.js', /*      + css    */
+        'scripts/vehicle.js', /*    + css    */
+        'scripts/points.js', /*     + css    */
+    ],
+    complete: function () {
+        if ($.browser.mozilla) {
+            W.setTimeout(function () {
+                $('#Platter td').wrapInner('<div class="relative">'); // HACK!
+                C.warn('fftd');
+            }, 999);
+        }
+    },
+}]);
